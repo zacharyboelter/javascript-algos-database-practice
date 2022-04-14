83973c23cbc9c082d5b904b08620d5cd402833c3
Zachary Boelter <zboelter@gmail.com>
2022-04-14 11:22:42 -0500

    random range function

--- app.js
+++ app.js
@@ -90,3 +90,9 @@ function lookUpProfile(name, prop) {
 }
 
 //   lookUpProfile("Akira", "likes");
+
+// Create a function called randomRange that takes a range myMin and myMax and returns a random whole number that's greater than or equal to myMin, and is less than or equal to myMax, inclusive.
+
+function randomRange(myMin, myMax) {
+    return Math.floor(Math.random() * (myMax - myMin + 1) + 1)
+}
